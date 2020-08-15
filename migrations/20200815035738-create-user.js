d5c4842ee32ba0d9188dd728c755b40acc0e41eb
subrataroy321 <subrata.r321@gmail.com>
2020-08-14 23:59:31 -0400

    [model] add model and migrate to db

---
+++ migrations/20200815035738-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      countryCode: {
+        type: Sequelize.INTEGER
+      },
+      contactNo: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,87 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user.init({
+    firstName: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1,50],
+          msg: 'Name must be 1 to 50 characters'
+        }
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1,30],
+          msg: 'Last Name must be 1 to 30 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8,20],
+          msg: 'Password must be between 8 to 20 characters'
+        }
+      }
+    },
+    countryCode: {
+      type: DataTypes.INTEGER,
+      validate: {
+        len: [1,6],
+        msg: 'Country Code should between 1 to 6 numbers'
+      }
+    },
+    contactNo: {
+      type: DataTypes.INTEGER,
+      validate: {
+        len: [10,10],
+        msg: 'Number must be 10 numbers'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+
+user.addHook('beforeCreate', function(pendingUser) {
+  let hash = bcrypt.hashSync(pendingUser.password, 12);
+  pendingUser.password = hash;
+})
+
+user.prototype.validPassword = function(passwordTyped) {
+  let correctPassword= bcrypt.compareSync(passwordTyped, this.password);
+  return correctPassword;
+}
+
+user.prototype.toJSON = function() {
+  let userData = this.get();
+  delete userData.password;
+  return userData;
+}
+return user;
+};
