e1758fa28771a8bac8336eafddf3882106a324ed
subrataroy321 <subrata.r321@gmail.com>
2020-08-16 00:37:45 -0400

    [auth.js] add post route for sign up

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -8,5 +9,35 @@ router.get('/signup', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('auth/login');
 });
+
+router.post('/signup', (req,res)=> {
+  if(req.body.password === req.body.confirmPassword) {
+    db.user.findOrCreate({
+      where: {email: req.body.email},
+      defaults: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        contactNo: req.body.contactNo,
+        password: req.body.password,
+        countryCode: req.body.countryCode
+      }
+    })
+    .then(([user,created])=> {
+      if(created) {
+        res.redirect('/');
+      } else {
+        res.redirect('/auth/signup')
+      }
+    })
+    .catch(error=> {
+      console.log('Error', error);
+      res.redirect('/auth/signup');
+    })
+
+  } else {
+    // sends a message thats password doesnot matchs with confirm password and stay on the same page
+    res.redirect('/auth/signup');
+  }
+})
 
 module.exports = router;
